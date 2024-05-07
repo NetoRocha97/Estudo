4db7f2c84f20dcff4d7a7a7390336e2cf44cb97c
Neto Rocha <63526723+NetoRocha97@users.noreply.github.com>
2024-05-07 19:49:44 -0300

    Exercicio de if-else e for (loops)

--- src/main/java/MeuPrograma.java
+++ src/main/java/MeuPrograma.java
@@ -6,9 +6,28 @@ public class MeuPrograma {
         proximaIdade = idade + 1;
         System.out.println("Minha idade é: " + idade + " e irei completar " + proximaIdade);
 
-        int i = 5;
+        /*int i = 5;
         int j = i;
         i = i + 1;
-        System.out.println(i + " e " + j);
+        System.out.println(i + " e " + j);*/
+
+        /*for (i = 150; i <= 1500; i++){
+            System.out.println(i);
+        }*/
+        /*for(i = 1; i <= 1000; i++){
+            System.out.println(i);
+        }*/
+        /*for(int i = 1; i <= 100; i++){
+            if(i % 3 == 0){
+                System.out.println(i);
+            }
+        }*/
+        for(int i = 1; i <= 100; i++){
+            if(i % 3 == 0){
+                System.out.println(i + " É múltiplo de 3");
+            } else {
+                System.out.println(i + " Não é múltiplo de 3");
+            }
+        }
     }
 }
